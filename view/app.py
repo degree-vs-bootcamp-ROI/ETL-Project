332c0583ed09e63a8a11e4c8266a883c641623f0
Karl Ramsay <nytechtiger@gmail.com>
2020-04-24 20:31:58 -0400

    file structure in place

---
+++ view/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template
+
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+dbconn = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = dbconn.indeed_db
+# Get source data for dynamically populated templates
+# source_df = pd.read_csv('indeed_cleaned.csv')
+
+jobs = []
+jobs = db.jobs.find().limit(300)
+# # Set index / set counter to 1
+# i = 1
+
+# Define routes
+@app.route("/")
+def welcome():
+    return render_template('index.html')
+
+
+@app.route("/graph.html")
+def Visualization():
+    return render_template('graph.html')
+
+
+@app.route("/data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('data.html', jobs=jobs)
+
+
+@app.route("/ny-data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('ny-data.html', jobs=jobs)
+
+
+@app.route("/il-data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('il-data.html', jobs=jobs)
+
+
+@app.route("/ma-data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('ma-data.html', jobs=jobs)
+
+
+@app.route("/ca-data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('ca-data.html', jobs=jobs)
+
+
+@app.route("/wa-data.html")
+def ViewData():
+    jobs = db.jobs.find().limit(50)
+    return render_template('wa-data.html', jobs=jobs)
+
+
+@app.route("/team.html")
+def About():
+    return render_template('team.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
